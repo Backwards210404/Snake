12c4f67fb969fe442c1317294dc8c06cc1692253
David <147860665+Backwards210404@users.noreply.github.com>
2024-05-12 01:57:48 +0200

    Create snake.py
    Create first Version

---
+++ snake.py
@@ -0,0 +1,47 @@
+import keyboard
+from turtle import *
+import time
+
+
+
+
+snake = Turtle()
+snake.shape('square')
+snake.color('green')
+snake.up()
+
+def checkdirection(direction, snake):
+    match (direction):
+        case("right"):
+            direction = "left"
+            return snake.left(90)
+        case("left"):
+            direction = "right"
+            return snake.right(90)
+            
+    
+def move(key, snake):
+    direction = "right"
+    snake.speed(0)
+    match (key):
+        case("w"):
+            checkdirection(direction, snake)
+            return snake.forward(50)
+        case("d"):
+            checkdirection(direction, snake)
+            return snake.forward(50)
+        case("s"):
+            checkdirection(direction, snake)
+        case("a"):
+            checkdirection(direction, snake)
+            return snake.forward(50)
+        case default:
+            return snake.forward(50)
+
+
+
+while True != False:
+    key = keyboard.read_key()
+    move(key, snake)
+    time.sleep(1)
+    
