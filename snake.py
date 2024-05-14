f10aa754eaee6af02e542d099992cd178623e47f
David <147860665+Backwards210404@users.noreply.github.com>
2024-05-14 12:31:12 +0200

    Food Moves

--- snake.py
+++ snake.py
@@ -1,18 +1,22 @@
 import turtle
+from random import *
 window = turtle.Screen()
 window.bgcolor('orange')
 window.tracer(0)
 
 Snake = turtle.Turtle("square")
 Snake.color('green')
-Snake.penup()   
+Snake.penup()
+   
 global direction
 direction = "right"
 
 def createfood():
-    food = turtle.Turtle("round")
+    global food
+    food = turtle.Turtle("circle")
     food.color('red')
     food.penup()
+    return food
 def up():
     global direction
     if direction != "down":
@@ -40,13 +44,13 @@ def right():
 def move(direction):
     match (direction):
         case ("right"):
-            Snake.forward(0.01)
+            Snake.forward(0.1)
         case("up"):
-            Snake.sety(Snake.ycor()+0.01)
+            Snake.sety(Snake.ycor()+0.1)
         case("down"):
-            Snake.sety(Snake.ycor()-0.01)
+            Snake.sety(Snake.ycor()-0.1)
         case("left"):
-            Snake.forward(-0.01)
+            Snake.forward(-0.1)
 
 window.onkeypress(up, "Up")
 window.onkeypress(down, "Down")
@@ -57,9 +61,17 @@ window.onkeypress(down, "s")
 window.onkeypress(right, "d")
 window.onkeypress(left, "a")
 print(window.listen())
+food = createfood()
+def is_collided_with(a, b):
+    return abs(a.xcor() - b.xcor()) < 10 and abs(a.ycor() - b.ycor()) < 10
+
 
 while True:
-  createfood()
-  move(direction)
-  window.update()
+    move(direction)
+    if is_collided_with(Snake, food):
+        food.goto(randint(0,200),randint(0,200))
+        
+        
+    window.update()
+  
   
